dcc8f7e73ae64e45dd49996651685c4c432fd395
Jim Fan <iamjimfan@gmail.com>
2019-11-24 12:25:25 +0800

    Remove unused code

--- main.c
+++ main.c
@@ -21,19 +21,6 @@ int main(int argc, char** argv)
         goto CLEAN_UP;
     }
 
-    /*
-    result = ncl_resolve_labels();
-    if (result == 0)
-    {
-        ncl_exec_inst();
-        ncl_dump_reg();
-    }
-    else
-    {
-        fprintf(stderr, "Aborted\n");
-    }
-    */
-
 CLEAN_UP:
     //ncl_cleanup();
     return result;
